d6800e3b160881121f88126d1c6ad54d842f983c
TheAnushkaTiwari <anushkatiwari8197@gmail.com>
2024-11-12 00:16:15 +0530

    Create Lab-2(2).py

---
+++ Lab-2/Lab-2(2).py
@@ -0,0 +1,10 @@
+num1=float(input("Enter 1st number"))
+num2=float(input("Enter 2nd number"))
+add=num1+num2
+sub=num1-num2
+multiply=num1*num2
+div=num1/num2
+print(add)
+print(abs(sub))
+print("%.2f" % multiply)
+print("%.2f" % div)
