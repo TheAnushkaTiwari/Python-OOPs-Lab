3e8c4e7ca4afe16a51949b6124e3ec1863288f7c
TheAnushkaTiwari <anushkatiwari8197@gmail.com>
2024-11-12 00:17:53 +0530

    Create Lab-2(4).py

---
+++ Lab-2/Lab-2(4).py
@@ -0,0 +1,8 @@
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))
+avg=(num1+num2)/2
+deviation1=num1-avg
+deviation2=num2-avg
+print(f"average is {avg}")
+print(f"deviation of {num1} is {deviation1}")
+print(f"deviation of {num2} is {deviation2}")
