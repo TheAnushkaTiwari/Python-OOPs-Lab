798e404a118b44286509f98323718cf6fa8a6fa7
TheAnushkaTiwari <anushkatiwari8197@gmail.com>
2024-11-12 00:44:52 +0530

    Create Lab-5(6).py

---
+++ Lab-5/Lab-5(6).py
@@ -0,0 +1,7 @@
+char="ObjectOrientedProgramming"
+u=set(char)
+lowercase_char=list(map(lambda x: x.upper(),u))
+uppercase_char=list(map(lambda x: x.lower(),u))
+print(lowercase_char)
+print()
+print(uppercase_char)
