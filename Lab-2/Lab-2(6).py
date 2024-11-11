ec02b742d14adbea00b9994f895687d5c1c6bcf5
TheAnushkaTiwari <anushkatiwari8197@gmail.com>
2024-11-12 00:19:30 +0530

    Create Lab-2(6).py

---
+++ Lab-2/Lab-2(6).py
@@ -0,0 +1,18 @@
+# This is a single-line comment
+# The following program demonstrates different types of comments in Python
+
+"""
+This is a multi-line comment.
+Useful for long explanations or disabling blocks of code.
+"""
+
+# Below is a simple program to add two numbers
+
+# Assigning values to variables
+a = 5  # First number
+b = 3  # Second number
+
+# Adding the two numbers and storing the result in 'result'
+result = a + b  # Storing sum of a and b in result
+
+print("The sum of", a, "and", b, "is:", result)  # Displaying the result
